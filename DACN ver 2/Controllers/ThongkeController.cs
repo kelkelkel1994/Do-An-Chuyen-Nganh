62678e6ae3e22b4b6eef98880940204d1dee35d8
Sơn Đoàn <sondoan1204@gmail.com>
2016-11-17 23:03:45 +0700

    xog 6 cái char

--- DACN ver 2/Controllers/ThongkeController.cs
+++ DACN ver 2/Controllers/ThongkeController.cs
@@ -55,5 +55,73 @@ namespace DACN_ver_2.Controllers
                        }).ToList();
             return Content(JsonConvert.SerializeObject(res));
         }
+        //Số lhop đồng ngoài trong tháng
+        public ContentResult GetSoHopDongNgoai()
+        {
+            var res = (from tags in data.HOPDONGs
+                       where tags.ID_LOAIHOPDONG == 1 //hop đồng ngoài
+                       group tags by tags.NGAYLAP.Value.Month into gp
+                       select new
+                       {
+                           thang = gp.Key,
+                           sl = gp.Count(),
+                       }).ToList();
+            return Content(JsonConvert.SerializeObject(res));
+        }
+        public ContentResult GetSoHopDongTrong()
+        {
+            var res = (from tags in data.HOPDONGs
+                       where tags.ID_LOAIHOPDONG == 2 //hop đồng trong
+                       group tags by tags.NGAYLAP.Value.Month into gp
+                       select new
+                       {
+                           thang = gp.Key,
+                           sl = gp.Count()
+                       }).ToList();
+            return Content(JsonConvert.SerializeObject(res));
+        }
+
+        //phieu yeu cau/tháng
+        public ContentResult GetPYCThang()
+        {
+            var res = (from tags in data.PHIEUYEUCAUs
+                       where tags.NGAYVIETPHIEU.Value.Year == DateTime.Now.Year
+                       group tags by tags.NGAYVIETPHIEU.Value.Month into gp
+                       select new
+                       {
+                           thang = gp.Key,
+                           //tennv = data.NHANVIENs.Single(d => d.ID_NHANVIEN == gp.Key).TENNV,
+                           sl = gp.Count()
+                       }).ToList();
+            return Content(JsonConvert.SerializeObject(res));
+        }
+        public ContentResult GetPYC5(string Id)
+        {
+            int nam = Convert.ToInt32(Id);
+            var res = (from tags in data.PHIEUYEUCAUs
+                       where tags.NGAYVIETPHIEU.Value.Year == nam
+                       group tags by tags.NGAYVIETPHIEU.Value.Month into gp
+                       select new
+                       {
+                           thang = gp.Key,
+                           sl = gp.Count()
+                       }).ToList();
+            return Content(JsonConvert.SerializeObject(res));
+        }
+
+        //doanh thu thực theo tháng
+        public ContentResult GetDoanhthuthuc()
+        {
+            var res = (from tags in data.HOPDONGs
+                       where tags.NGAYLAP.Value.Year == DateTime.Now.Year
+                       group tags by tags.NGAYLAP.Value.Month into gp
+                       select new
+                       {
+                           thang = gp.Key,
+                           dt = gp.Sum(x => x.DOANHTHUTHUC)
+                       }).ToList();
+
+            return Content(JsonConvert.SerializeObject(res));
+        }
     }
 }
